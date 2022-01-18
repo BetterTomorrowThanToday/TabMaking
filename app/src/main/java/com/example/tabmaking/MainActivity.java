4e2e353a2acc2a14928159844bb9ef63a253796a
BetterTomorrowThanToday <wsx0239@gmail.com>
2022-01-18 17:46:36 +0900

    준혁이 두번째 탭 합치기

--- app/src/main/java/com/example/tabmaking/MainActivity.java
+++ app/src/main/java/com/example/tabmaking/MainActivity.java
@@ -1,36 +1,79 @@
 package com.example.tabmaking;
 
-import androidx.annotation.NonNull;
-import androidx.core.view.ViewCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends FragmentActivity {
+
+public class MainActivity extends FragmentActivity{
 
     private ViewPager2 mPager;
     private FragmentStateAdapter pagerAdapter;
     private int num_page = 3;
+
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //1번 탭 미리 표시
+        firstView();
+        secondView();
+        recyclerView.setVisibility(View.INVISIBLE);
+        //Tab implant
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition();
+                changeView(pos);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+        }) ;
+    }
+
+    private void changeView(int index) {
+
+        switch (index) {
+            case 0:
+                mPager.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.INVISIBLE);
+                break;
+            case 1:
+                recyclerView.setVisibility(View.VISIBLE);
+                mPager.setVisibility(View.INVISIBLE);
+                break;
+        }
+    }
+
+    private void firstView() {
         //ViewPager2
         mPager = findViewById(R.id.viewpager);
         //Adapter
         pagerAdapter = new MyAdapter(this, num_page);
         mPager.setAdapter(pagerAdapter);
-
         //ViewPager Setting
         mPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
         mPager.setCurrentItem(1000);
         mPager.setOffscreenPageLimit(3);
-//Change in position -> change in view page
+        //Change in position -> change in view page
         mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -40,6 +83,23 @@ public class MainActivity extends FragmentActivity {
                 }
             }
         });
+    }
+    private void secondView() {
+        recyclerView = findViewById(R.id.recycler_view);
+        itemAdapter = new ItemAdapter();
+        recyclerView.setAdapter(itemAdapter);
+        //조회 전 화면 클리어
+        itemAdapter.removeAllItem();
+        //샘플 데이터 생성
+        for(int i = 0; i < 20; i++){
+            Item item = new Item();
+            //데이터 등록
+            itemAdapter.addItem(item);
+        }
+        //적용
+        itemAdapter.notifyDataSetChanged();
+        //애니메이션 실행
+        recyclerView.startLayoutAnimation();
     }
 }
 
